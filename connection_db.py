e44b9df3aa4554a52477955bcdeccc3da4926ddb
vinivcr <104727390+vinivcr@users.noreply.github.com>
2024-11-07 13:17:07 -0300

    Update connection_db.py

--- connection_db.py
+++ connection_db.py
@@ -3,7 +3,7 @@ import pymysql
 # Conectando ao banco de dados AWS RDS
 def get_connection():
   return pymysql.connect(
-  host='15.228.100.136',
+  host='localhost',
   user='telecom_user',
   password='4Wb42T%zkG6Q',
   database='telecom_db',
